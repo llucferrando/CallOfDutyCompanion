8930db92aa3ad21fff352addf91bb4c362d8f76a
Lluc Ferrando Jordà <lluc.ferrando@enti.cat>
2023-12-20 11:48:51 +0100

    Logo included

--- CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/activities/MainActivity.kt
+++ CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/activities/MainActivity.kt
@@ -3,12 +3,17 @@ package com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.activities
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.R
 import com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.clases.firebaseclasses.FB
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+
+
+
         setContentView(R.layout.main_activity_screen)
 
         FB
