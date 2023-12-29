28216492c96ba037ab0d74d408e98b92bbe97cc5
Lluc Ferrando Jordà <lluc.ferrando@enti.cat>
2023-12-29 18:56:26 +0100

    Contact screen done pèndent to logic

--- CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/clases/fragments/screens/ContactUs.kt
+++ CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/clases/fragments/screens/ContactUs.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.R
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textfield.TextInputLayout
 
 class ContactUs : Fragment(){
     lateinit var fragmentView: View
+    val contactPhone by lazy { fragmentView.findViewById<MaterialCardView>(R.id.phoneContact) }
+    val contactMail by lazy { fragmentView.findViewById<TextInputLayout>(R.id.emailContact) }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,4 +21,24 @@ class ContactUs : Fragment(){
         fragmentView = inflater.inflate(R.layout.contactus_screen, container,false)
         return fragmentView
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        contactPhone.setOnClickListener { contactUsByPhone() }
+        contactMail.setOnClickListener { contactUsByMail() }
+    }
+    private fun contactUsByPhone(){
+        contactPhone.setOnClickListener {
+
+        }
+
+    }
+    private fun contactUsByMail(){
+        contactMail.setOnClickListener {
+
+        }
+
+    }
+
 }
