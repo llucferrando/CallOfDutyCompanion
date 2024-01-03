89cb5c683869339e8edd837d630e72502efc07b7
Lluc Ferrando Jordà <lluc.ferrando@enti.cat>
2024-01-03 14:52:39 +0100

    Notifications done

--- CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/activities/MainActivity.kt
+++ CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/activities/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 import com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.R
 import com.enti.dostres.cdi.llucferrando.pauinsa.callofdutycompanion.clases.firebaseclasses.FB
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    private val requestNotificationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted->
+        onNotificationPermissionResponse(isGranted)
+    }
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
         Thread.sleep(2000)
-
-
 
         setContentView(R.layout.main_activity_screen)
 
@@ -24,7 +32,40 @@ class MainActivity : AppCompatActivity() {
                 key("Level Of Subnromality", 1900)
             }
         setTheme(FB.remoteConfig.getTheme().themeId)
+        askNotificationPermission()
     }
 
+    fun onNotificationPermissionResponse(isGranted:Boolean){
+        if(isGranted){
+             FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+                Log.d("Token","Token::> " + token)
 
+            }.addOnFailureListener {
+                //TODO ERROR
+             }
+
+
+        }else{
+            //TODO ERROR
+        }
+    }
+
+    fun askNotificationPermission(){
+        val permission = Manifest.permission.POST_NOTIFICATIONS
+
+        if(ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
+            FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+                Log.d("Token","Token::> " + token)
+
+            }.addOnFailureListener {
+                //TODO ERROR
+            }
+        }else if(shouldShowRequestPermissionRationale(permission)){
+            //Move to setup screen
+        }else{
+            requestNotificationPermissionLauncher.launch(permission)
+        }
+
+
+    }
 }
