a115a4f1180921932c9f812306fd11c93fdda041
Lluc Ferrando Jordà <lluc.ferrando@enti.cat>
2024-01-09 21:38:10 +0100

    Last last last of the last fix

--- CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/activities/MainActivity.kt
+++ CallOfDutyCompanion/app/src/main/java/com/enti/dostres/cdi/llucferrando/pauinsa/callofdutycompanion/activities/MainActivity.kt
@@ -21,8 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
         Thread.sleep(2000)
-
-
+        setContentView(R.layout.main_activity_screen)
         FB
             .crashalytics
             .logSingleError("Error2"){
